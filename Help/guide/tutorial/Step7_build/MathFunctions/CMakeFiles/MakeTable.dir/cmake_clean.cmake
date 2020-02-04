c19b8c630c14feedc097947c5e8d124bc9835d16
Todd Flanders <todd.a.flanders@accenture.com>
2020-02-04 11:23:16 -0500

    finish step 7

---
+++ Help/guide/tutorial/Step7_build/MathFunctions/CMakeFiles/MakeTable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MakeTable.dir/MakeTable.cxx.o"
+  "MakeTable"
+  "MakeTable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MakeTable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
